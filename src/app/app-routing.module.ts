bb85748757860f31670d00ff109d317e54c0f632
Marcos Rogério Defina Junior <marcosdefina@gmail.com>
2019-04-04 14:29:58 -0300

    templating components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { HelpComponent } from './help/help.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'help', component: HelpComponent },
 ];
 
 @NgModule({
