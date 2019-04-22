b3fe29bc6ee94c5c6ed9e99b806206e3d87bc016
Marcos Defina <marcosdefina@gmail.com>
2019-04-22 11:07:21 -0300

    adding offcanvas template

---
+++ src/app/subcomponents/offcanvas/offcanvas.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    selector: 'offcanvas',
+    templateUrl: './offcanvas.component.html',
+    styleUrls: ['./offcanvas.component.css'],
+})
+
+export class OffcanvasComponent {
+    @Input() offcanvasContent: string;
+    @Input() contentLink: string;
+}
