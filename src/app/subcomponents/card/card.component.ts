ca16cdfda2730a7749fb2c4de07ecf0031acbbad
Vitor Ponce <vponcee30@gmail.com>
2019-05-10 15:29:44 -0300

    fixed some issues of typeScript in card.component.ts

--- src/app/subcomponents/card/card.component.ts
+++ src/app/subcomponents/card/card.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input } from '@angular/core';
 
 @Component({
-    selector: 'card',
+    selector: 'app-card',
     templateUrl: './card.component.html',
     styleUrls: ['./card.component.css'],
 })
