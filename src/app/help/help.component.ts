abfb97bfe3edd77b75bc9527197feb137430b991
MarcosDefina <marcosdefina@gmail.com>
2019-05-22 16:25:26 -0300

    complete about page, design and functionality

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import M from 'materialize-css';
 
 @Component({
@@ -7,15 +7,35 @@ import M from 'materialize-css';
   styleUrls: ['./help.component.css']
 })
 export class HelpComponent implements OnInit {
-
+  @ViewChild('intro') public intro: ElementRef;
+  @ViewChild('version') public version: ElementRef;
+  @ViewChild('req') public req: ElementRef;
+  @ViewChild('nav') public nav: ElementRef;
   constructor() { }
 
   ngOnInit() {
-    document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', function () {
       var elems = document.querySelectorAll('.fixed-action-btn');
-      var instances = M.FloatingActionButton.init(elems, Option);
+      var instances = M.FloatingActionButton.init(elems, {
+        hoverEnabled: false
+      });
     });
-  }   
+  }
 
-
+  scrollTo(id){
+    switch(id){
+      case 'intro':
+        this.intro.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' });
+        break;
+      case 'version':
+        this.version.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' });
+        break;
+      case 'req':
+        this.req.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' });
+        break;
+      case 'nav':
+        this.nav.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' });
+        break;
+    }
+  }
 }
